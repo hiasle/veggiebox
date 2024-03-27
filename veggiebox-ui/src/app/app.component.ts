fc835fe8a9d67b39455d0e9d37501c665cfad874
Matthias Huber <matthias.hube@gmail.com>
2024-03-27 15:35:10 +0100

    Default pipes format (currency, date)

--- veggiebox-ui/src/app/app.component.ts
+++ veggiebox-ui/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
 import { ApiModule, BASE_PATH } from './openapi';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, SidebarComponent, ApiModule],
-  providers: [{ provide: BASE_PATH, useValue: 'http://localhost:8080' }],
+  providers: [
+    { provide: BASE_PATH, useValue: 'http://localhost:8080' },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: 'dd.MM.yyyy' },
+    },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
